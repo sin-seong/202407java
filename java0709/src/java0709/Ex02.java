4a0115d50f0f661995727affcf20005d75d212ab
sin-seong <leeshoma@gmail.com>
2024-07-09 16:13:35 +0900

    0708 학습

---
+++ java0709/src/java0709/Ex02.java
@@ -0,0 +1,24 @@
+package java0709;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		float f1 = 3.14f;
+		double d1 = 314e-2;
+		System.out.println(d1);
+		
+		//int sum ;
+		//int score1 = 0;
+		//int score2;
+		//sum = score1 + score2;
+		
+		int x1 = 100;
+		if (true) {
+			int x2 = 300;
+			System.out.println(x1);
+		}
+		
+		//System.out.println(x2);
+	}
+
+}

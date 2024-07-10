1e7a44bab5fa4cc9f6802fcb97fbc977df36a605
sin-seong <leeshoma@gmail.com>
2024-07-10 17:49:39 +0900

    0710

---
+++ java0710/src/java0710/text14.java
@@ -0,0 +1,25 @@
+package java0710;
+
+import java.util.Scanner;
+
+public class text14 {
+
+	public static void main(String[] args) {
+		boolean run = true;
+		int bals = 0;
+		int y = 0;
+		int z = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		while(run) {
+			System.out.println("\n---------------------------------");
+			System.out.println("1.로그인 | 2. 회원가입 | 3.예.출 | 4. 종료");
+			System.out.println("---------------------------------");
+			System.out.printf("선택> ");
+			
+			
+		}
+
+	}
+
+}

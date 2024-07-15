831a5248ebbe38cb2e1ab1ee5eaea82cc62d36a3
sin-seong <leeshoma@gmail.com>
2024-07-15 17:47:51 +0900

    0715 학습

---
+++ java0715/src/dp2/Course.java
@@ -0,0 +1,34 @@
+package dp2;
+
+import java.util.Vector;
+
+public class Course {	
+	private Vector<Transcript> transcripts;
+	private String name;
+	
+	
+	public Course(String name) {
+		super();
+		this.transcripts = new Vector<Transcript>();
+		this.name = name;
+	}
+	public void addTranscript(Transcript transcript) {
+		transcripts.add(transcript);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Course [transcriptss=" + transcripts + ", name=" + name + "]";
+	}
+	
+	public void setTranscrpts(Vector<Transcript> transcript) {
+		this.transcripts = transcripts;
+	}
+	
+}
